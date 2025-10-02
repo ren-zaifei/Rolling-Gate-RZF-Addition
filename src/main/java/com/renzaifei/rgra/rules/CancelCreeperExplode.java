4254957f7c8349a77dd4905d919691d8ee0c0a61
人哉肥 <chuen6@qq.com>
2025-10-02 22:10:01 +0800

    新增规则---苦力怕禁止破坏方块

---
+++ src/main/java/com/renzaifei/rgra/rules/CancelCreeperExplode.java
@@ -0,0 +1,20 @@
+package com.renzaifei.rgra.rules;
+
+
+import com.renzaifei.rgra.RollingGateRZFAddition;
+import com.renzaifei.rgra.RollingGateRZFAdditionServerRules;
+import net.minecraft.world.entity.monster.Creeper;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.level.ExplosionEvent;
+import org.jetbrains.annotations.NotNull;
+
+@EventBusSubscriber(modid = RollingGateRZFAddition.MODID)
+public class CancelCreeperExplode {
+    @SubscribeEvent
+    public static void cancel(@NotNull ExplosionEvent.Start event) {
+        if (!RollingGateRZFAdditionServerRules.cancelCreeperExplode) return;
+        if (!(event.getExplosion().getDirectSourceEntity() instanceof Creeper)) return;
+        event.setCanceled(true);
+    }
+}
